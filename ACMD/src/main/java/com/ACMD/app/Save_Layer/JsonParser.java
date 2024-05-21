feb99d648e3fca2672397a2a2cb665070dec79b9
PdP03 <40737069+PdP03@users.noreply.github.com>
2024-05-21 16:36:19 +0200

    Gson parser first commit

---
+++ ACMD/src/main/java/com/ACMD/app/Save_Layer/JsonParser.java
@@ -0,0 +1,15 @@
+package com.ACMD.app.Save_Layer;
+
+import java.io.Writer;
+
+import com.google.gson.Gson;
+
+public class JsonParser {
+    
+    public void serializeObj(Object obj)
+    {   
+
+        Gson serializeObj = new Gson();
+
+    }
+}
