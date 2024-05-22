f4dba005e83e18fb2b1135e64a7089340a87fe09
CarloMaggi <169063847+CarloMaggi@users.noreply.github.com>
2024-05-22 15:16:32 +0200

    Create ItemFactory.java VUOTO

---
+++ ACMD/src/main/java/com/ACMD/app/Engine_Layer/StorageManagement/ItemFactory.java
@@ -0,0 +1,10 @@
+
+package com.ACMD.app.Engine_Layer.StorageManagement;
+
+public class ItemFactory
+{
+    public ItemStack getRandomItem()
+     {return null;}
+    public ItemStack getItem(ItemType t)
+     {return null;}
+}
