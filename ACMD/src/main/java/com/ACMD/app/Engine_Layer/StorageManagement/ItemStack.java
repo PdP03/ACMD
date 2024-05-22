d66b7eb621c820e81861b3a7dcefbca2ee1f0611
CarloMaggi <169063847+CarloMaggi@users.noreply.github.com>
2024-05-22 13:47:23 +0200

    Update ItemStack.java ; not final version

--- ACMD/src/main/java/com/ACMD/app/Engine_Layer/StorageManagement/ItemStack.java
+++ ACMD/src/main/java/com/ACMD/app/Engine_Layer/StorageManagement/ItemStack.java
@@ -16,7 +16,9 @@ public /*abstract*/ class ItemStack
     String nome;
     int peso;
     int quantita;           //oggetti si possono impilare
+    int valore;             //#TERMINARE
     ItemType tipologia;
+    String description;     //#TERMINARE
 
     final int VALORE_DEFAULT_QUANTITA = 1;
 
@@ -24,53 +26,65 @@ public /*abstract*/ class ItemStack
 
     public ItemStack(String n, ItemType t, int p)
     {
-        if(p<0)
-         throw new IllegalArgumentException("Peso non può essere negativo");  //compilatore mette condition guards come false in cache di default?
-        if(n=="" || n==null)
-         throw new IllegalArgumentException("Il nome dell'oggetto non è valido");
-
         nome= n;
         peso= p;
         quantita= VALORE_DEFAULT_QUANTITA;
         tipologia= t;
+
+        exceptionLauncher();
     }
 	    	
     public ItemStack(String n, ItemType t, int p, int q  )
     {
-        if( p<0)
-         throw new IllegalArgumentException("Peso non può essere negativo");
-        if(n=="" || n==null)
-         throw new IllegalArgumentException("Il nome dell'oggetto non è valido");
-        if(q <=0)
-         throw new IllegalArgumentException("La quantità non è positiva");
-
         nome= n;
         peso= p;
         quantita= q;
         tipologia= t;
+
+        exceptionLauncher();
+    }
+
+//  ## Metodi Private ##
+
+    private void exceptionLauncher()
+    {
+        if( peso<0)
+         throw new IllegalArgumentException("Peso non può essere negativo");
+        if(nome=="" || nome==null)
+         throw new IllegalArgumentException("Il nome dell'oggetto non è valido");
+        if(quantita <=0)
+         throw new IllegalArgumentException("La quantità non è positiva");
     }
 
 
-//  ## Metodi inline ##
+//  ## Metodi Public ##
 
-    public int getQuantity()
-     { return quantita; }
-
-    public void addItem()
+    public void addQuantity()
      { ++quantita; }
 
-    public ItemType getType()
-     { return tipologia;}
-
-    public boolean removeItem()
+    public boolean removeQuantity()
      {
         if( quantita>0 ) return --quantita != 0;
          else throw new noItem_Exception();
      }
 
+
+//  ## Metodi variabili ##
+
+    public int getQuantity()
+     { return quantita; }
+    public int getValue()
+     { return valore; }
+    public ItemType getType()
+     { return tipologia;}
     public int getWeight()
      { return peso;}
+    public String getName()
+     {return nome;}
 
-    public String toString()
-     { return nome; }
+    public String showDescription()
+     {return description;}
+
+/*    public String toString()
+     { return nome; }*/
 }
