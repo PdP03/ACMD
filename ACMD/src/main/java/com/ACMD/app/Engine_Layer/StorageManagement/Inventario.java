c4f67c145cd6ebba5a0f472707eb220c5b5b6528
CarloMaggi <169063847+CarloMaggi@users.noreply.github.com>
2024-05-22 15:15:42 +0200

    Update Inventario.java TERMINARE

--- ACMD/src/main/java/com/ACMD/app/Engine_Layer/StorageManagement/Inventario.java
+++ ACMD/src/main/java/com/ACMD/app/Engine_Layer/StorageManagement/Inventario.java
@@ -1,74 +1,24 @@
-/*
-    Per aumentare l'efficienza si potrebbe fare un bucket che divide per le varie categorie
-     di tipo di arma.
-    Poi all'interno del bucket fare una sorta di merge sort.. magari con un albero
 
-    Ricorda che:
-        -la classe Item rappresenta uno stack di oggetti, quindi inventario deve contenere
-        tutte istanze di oggetti diversi : nel test posso fare questo controllo per vedere
-        se per qualche motivo creare più istanze dello stesso oggetto
-*/
-
-// TERMINARE : dover mettere che togliere il peso quando fa remove
-// TERMINARE : forse ciò che sto facendo non funziona, perché io non salvo direttamente l'elemento, quindi non posso ritrovarmelo nella lista
-// TERMINARE : se salva più oggetti di stesso tipo in inventario
 package com.ACMD.app.Engine_Layer.StorageManagement;
 
-import java.util.LinkedList;
+public class Inventario extends Storage
+{
+    byte totalWeight;
 
-public class Inventario extends Storage {
-    // ## Variabili ##
+//  ## Costruttori ##
 
-    byte pesoAttuale = 0; // inventario conosce solo il suo peso, non quanto può portarne il personaggio
-    byte maxOggetti, contOggetti = 0;
+//  ## Metodi Public ##
 
-    // ## Costruttore ##
-    public Inventario(byte maxOggetti) {
-        super();
-        if (maxOggetti <= 0)
-            throw new IllegalArgumentException();
+   ItemStack searchFor(String item)
+    {return null;}
 
-        this.maxOggetti = maxOggetti;
-    }
 
-    // ## Metodi ##
 
-    public boolean addItem(Item t) {
-        if (++contOggetti == maxOggetti) // NOTA: in realtà avrei dovuto fare un metodo che controlla se è full, e poi
-                                         // usare quello; inoltre avrei dovuto lanciare una eccezione.. ma così secondo
-                                         // me è più efficiente
-        {
-            --contOggetti; // torna a stato valido
-            // throw new InventoryOutOfBound("Non ci sono posti per altri tipi di oggetti");
-            return false;
-        }
+//  ## Metodi return variabili ##
 
-        LinkedList<Item> items = super.showStorage();
-        int pos = items.indexOf(t);
-
-        if (pos < 0)
-            items.add(t); // aggiungo oggetto che non è in inventario
-        else {
-            t = items.get(pos);
-            t.addItem();
-            pesoAttuale += t.getWeight();
-        }
-
-        return true;
-    }
-
-    public byte getWeigth() {
-        return pesoAttuale;
-    }
-
-    public int quantityOf( Item t )         //restituisce la quantità di un determinato oggetto
-    {
-        LinkedList<Item> items = super.showStorage();
-        /*int pos = item.indexOf(t);
-        t = items.get( pos<0 ? return 0; : pos );   //non esiste un metodo che mi permette di crea
-        return t.getQuantity(); */
-
-        return (items.contains(t) ? t.getQuantity() : 0);
-    }
+    public byte getWeight()
+     {return totalWeight;}
+    public byte quantyOf(String items)
+     {return 0;}
 
 }
