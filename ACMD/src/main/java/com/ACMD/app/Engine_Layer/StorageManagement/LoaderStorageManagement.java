c7f2b40e5bd0566ea62d46cc23ff9252093bbb07
CarloMaggi <169063847+CarloMaggi@users.noreply.github.com>
2024-05-22 15:17:47 +0200

    Create LoaderStorageManagement.java VUOTO

---
+++ ACMD/src/main/java/com/ACMD/app/Engine_Layer/StorageManagement/LoaderStorageManagement.java
@@ -0,0 +1,11 @@
+
+package com.ACMD.app.Engine_Layer.StorageManagement;
+
+public class LoaderStorageManagement
+{
+
+    public LoaderStorageManagement(String file)
+     {}		//#UML : correggere il nome in UML
+    public Inventario getInventory()
+     {return null;}
+}
