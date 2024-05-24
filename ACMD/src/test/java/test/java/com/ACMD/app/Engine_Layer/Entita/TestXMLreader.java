47b1daecdd98e962322a962e9f76c0f7adb48188
X3laRam <169063754+X3laRam@users.noreply.github.com>
2024-05-24 22:17:09 +0200

    aggiunto test per xmlReader

---
+++ ACMD/src/test/java/test/java/com/ACMD/app/Engine_Layer/Entita/TestXMLreader.java
@@ -0,0 +1,45 @@
+package test.java.com.ACMD.app.Engine_Layer.Entita;
+
+import java.util.Vector;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.ACMD.app.Engine_Layer.Entita.MType;
+import com.ACMD.app.Engine_Layer.Entita.MonsterValues;
+import com.ACMD.app.Engine_Layer.Entita.xmlReader;
+
+public class TestXMLreader {
+    @Test
+    public void XMLReader(){
+        xmlReader reader = new xmlReader("MonsterConfig.xml");
+
+        Vector<MonsterValues> values = reader.getMonsterValues();
+
+        // ---------- TEST MOSTRO MARINO ----------
+        Assert.assertEquals(3, values.get(MType.MOSTRO_MARINO.getId()).health);
+        Assert.assertEquals(1, values.get(MType.MOSTRO_MARINO.getId()).armor);
+        Assert.assertEquals(2, values.get(MType.MOSTRO_MARINO.getId()).healthMul);
+
+        // ---------- TEST DRAGO ----------
+        Assert.assertEquals(3, values.get(MType.BOSS_DRAGO.getId()).health);
+        Assert.assertEquals(1, values.get(MType.BOSS_DRAGO.getId()).armor);
+        Assert.assertEquals(2, values.get(MType.BOSS_DRAGO.getId()).healthMul);
+
+        // ---------- TEST ARMATURA ----------
+        Assert.assertEquals(3, values.get(MType.ARMATURA.getId()).health);
+        Assert.assertEquals(1, values.get(MType.ARMATURA.getId()).armor);
+        Assert.assertEquals(2, values.get(MType.ARMATURA.getId()).healthMul);
+
+        // ---------- TEST COBOLDO ----------
+        Assert.assertEquals(3, values.get(MType.COBOLDO.getId()).health);
+        Assert.assertEquals(1, values.get(MType.COBOLDO.getId()).armor);
+        Assert.assertEquals(2, values.get(MType.COBOLDO.getId()).healthMul);
+
+        // ---------- TEST MAGO ----------
+        Assert.assertEquals(3, values.get(MType.MAGO_OSCURO.getId()).health);
+        Assert.assertEquals(1, values.get(MType.MAGO_OSCURO.getId()).armor);
+        Assert.assertEquals(2, values.get(MType.MAGO_OSCURO.getId()).healthMul);
+    }
+    
+}
