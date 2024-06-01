31383d3eeeadf79cb2c3897bbe7c024bb4ceedcd
padrinma <164861172+PadrinMa@users.noreply.github.com>
2024-06-01 13:50:19 +0200

    Ho pulito il codice di move

--- ACMD/src/main/java/com/ACMD/app/Graphic_Layer/GUI/GameFrame.java
+++ ACMD/src/main/java/com/ACMD/app/Graphic_Layer/GUI/GameFrame.java
@@ -14,193 +14,90 @@ import com.ACMD.app.Engine_Layer.Mappa.MapGraph;
 
 
 public class GameFrame extends javax.swing.JFrame implements Frame {
-    int globalRiga=0;
-    int globalColonna=0;
+    //Inizializzazione delle variabili del frame ( per altre, più complesse guardare initComponents() )
+    int globalRiga=0; //Usate per il test
+    int globalColonna=0; // Usate per i test
     
     Color backGround = new Color(150,150,150);
     Color lightBackground = new Color(204, 204, 204);
+
     ParsePath pathParser=new ParsePath();
     final String imageDirPath = "\\ACMD\\src\\main\\java\\com\\ACMD\\app\\Graphic_Layer\\Images\\";
     boolean youWin=false;
+    String mapIconPath     = ParsePath.getPath(imageDirPath, "Mappa_Definitiva.png");
+    String mapIconPathOpen = ParsePath.getPath(imageDirPath, "Mappa_DefinitivaOpen.png");
     
-    String mapIconPath = pathParser.getPath(imageDirPath, "Mappa_Definitiva.png");
-    String mapIconPathOpen = pathParser.getPath(imageDirPath, "Mappa_DefinitivaOpen.png");
-    
-    String playerIcon  = pathParser.getPath(imageDirPath,"PersonIcon.jpg");
-    String keyIcon     = pathParser.getPath(imageDirPath,"Key.jpg");
-    String musicIcon   = pathParser.getPath(imageDirPath,"Music.png");
+    String playerIcon      = ParsePath.getPath(imageDirPath,"PersonIcon.jpg");
+    String keyIcon         = ParsePath.getPath(imageDirPath,"Key.jpg");
+    String musicIcon       = ParsePath.getPath(imageDirPath,"Music.png");
+    final int numberRows   =20;
+    final int numberCols   =20; 
+    final float fontSize   =16f;
     /**
-     * Costruttore che genera il gameFrame, ne setta il background 
+     * Costruttore che genera il gameFrame, inizializza le compontenti, ne setta il background 
      * 
      */
     public GameFrame() {
                     this.getContentPane().setBackground(backGround);
                     initComponents(); // Crea la finestra vuota, senza aggiugnere la mappa          
-                    addMapPicture(mapIconPath); // Aggiunge l'immagine (grafica)
-                 //   addPlayerPosition(1,19,playerIcon); 
-                    move(1,19);
-    }
-                      
-    
-    /**
-     * Metodo che inizializza tutte le componenti del frame
-     */
-    public void initComponents() {
-        //Inizializzazione delle variabili oggetto per la grafica 
-        //setBackground(new java.awt.Color(0,0,0));
-        jToolBar1 = new javax.swing.JToolBar();
-        jScrollPane4 = new javax.swing.JScrollPane();
-        jInternalFrame1 = new javax.swing.JInternalFrame("Caverna");
-        jButtonPlayer = new javax.swing.JRadioButton();
-        jLabelMap = new javax.swing.JLabel();
-        jPanelProgressBars = new javax.swing.JPanel(); jPanelProgressBars.setBackground(backGround);
-        jBarVitaNemico = new javax.swing.JProgressBar();
-        jLabel1 = new javax.swing.JLabel();
-        jLabelVita = new javax.swing.JLabel();
-        jBarVitaPlayer = new javax.swing.JProgressBar();
-        jLabel2 = new javax.swing.JLabel();
-        jBarPeso = new javax.swing.JProgressBar();
-        jPanel2 = new javax.swing.JPanel();
-        jButtonInvio = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextStory = new javax.swing.JTextArea();
-        jTextComandi = new javax.swing.JTextField();
-
-        jToolBar1.setRollover(true);
-
-        //Implementazione dell'inventario
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setResizable(true);
-        jLabel1.setText("Nemico");
-        jLabelVita.setText("Vita");
-        jLabel2.setText("Peso");
-        jInternalFrame1.setVisible(true);
-        jButtonPlayer.setEnabled(false);    
-        jBarPeso.setBackground(lightBackground);
-        jBarVitaNemico.setBackground(lightBackground);
-        jBarVitaNemico.setForeground(new Color(255,0,0));
-        jBarVitaPlayer.setBackground(lightBackground);
-        jBarVitaNemico.setForeground(new Color(255,0,0));
-
-        jBarPeso.setForeground(new java.awt.Color(0, 255, 0));
-        jBarPeso.setValue(10);
-
-
-        AddComponents1(); //Metodo black box
-        
-        jTextComandi.addFocusListener(new FocusListener() {
-            @Override
-            public void focusGained(FocusEvent e) {
-                if (jTextComandi.getText().equals("Enter your answer")) {
-                    jTextComandi.setText("");
-                }
-            }
-
-            @Override
-            public void focusLost(FocusEvent e) {
-                if (jTextComandi.getText().isEmpty()) {
-                    jTextComandi.setText("Enter your answer");
-                }
-            }
-        });
-
-
-        jButtonInvio.setText("Submit");
-        jButtonInvio.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                
-                try {
-                    if(jTextComandi.getText().contains("Open"))
-                    {
-                        String[] t=jTextComandi.getText().split(",");
-                        int x=Integer.parseInt(t[0]);
-                        int y=Integer.parseInt(t[1]);
-                        MapGraph.setBeaten(new Coordinates(x,y));
-                        key.setVisible(false);
-                        key.setText(MapGraph.keys+"");
-                        key.setVisible(true);                        
-                    }
-                    String[] t=jTextComandi.getText().split(",");
-                    int x=Integer.parseInt(t[0]);
-                    int y=Integer.parseInt(t[1]);
-                    move(x,y);
-                }catch (Exception e ){}
-                
-                SwingUtilities.updateComponentTreeUI(jInternalFrame1);
-                
-           }
-        });
-
-        jTextStory.setBackground(new Color(0,0,0));
-        jTextStory.setForeground(new Color (255,255,255));
-        jTextStory.setFont(jTextStory.getFont().deriveFont( 16f));
-        jTextStory.setColumns(20);
-        jTextStory.setRows(5);
-        jTextStory.setText(MapGraph.getAllIcons());
-        jScrollPane1.setViewportView(jTextStory);
-
-        jTextComandi.setText("Enter your answer");
-
-        addComponents2();
-        pack();
-    }// </editor-fold>                        
-                
-    
+                    //addMapPicture(mapIconPath); // Aggiunge l'immagine (grafica)
+                    //addPlayerPosition(1,19,playerIcon); 
+                    move(new Coordinates(1,19));
+                    updateGraphics(new Coordinates(1,19));
+    }                   
     /**
      * Moves the player position from the current one to the desired one: 
      * @param x The position from the top left corner along the x axis, values 0 to 19
      * @param y The position from the top left corner along the y axis, values 0 to 19
-     * @throws RuntimeException x or y not in the range [0,19]
+     * @throws IllegalArgumentException x or y not in the range [0,19]
      */
-    public void move (int x,int y) throws IllegalArgumentException
-        {
-           // System.out.println("Move chiamato con" +x+ " "+y);
+    public void move (Coordinates c) throws IllegalArgumentException
+    {
+        int x=c.getX(); 
+        int y=c.getY();
         if(x <0 || x>19 || y<0 || y>19 ) throw new IllegalArgumentException("either x or y values not between 0-19 ");
-        jInternalFrame1.remove(jLabelMap);
+        jInternalFrame1.remove(jLabelMap); //jInternalFrame è il frame in cui c'è la mappa
         jLabelMap=new JLabel("");
-        try{
-            if(MapGraph.keys==2) 
+        try
+        { //TODO: Spostare il numero di chiavi a 4 
+            if(MapGraph.keys==2) //TODO Se ho 4 chiavi allora devo aprire la porta del drago spostare 2 a 4 
             {
                 MapGraph.setDragon();
             }  
-            String path = MapGraph.getIconOf(new Coordinates(x,y));
-            path = pathParser.getPath(imageDirPath,path );
-            jLabelMap.setIcon(new ImageIcon(path));
-        }catch(Exception e ){}
-      
-        jLabelMap.setSize(jInternalFrame1.getSize());
-     //   jLabelMap.setIcon(new ImageIcon("C:\\Users\\Matteo\\Desktop\\Test_git\\ACMD\\ACMD\\src\\main\\java\\com\\ACMD\\app\\Graphic_Layer\\Images\\Mappa_Definitiva.png"));
-        jLabelMap.setLayout(new GridLayout(20,20));
-        jButtonPlayer.setEnabled(false);    
-        
-            //Aggiungo le label e il pulsante del player nella posizione corretta
+            jLabelMap.setSize(jInternalFrame1.getSize());               //Ridimensiona 
+            jLabelMap.setLayout(new GridLayout(numberRows,numberCols)); // Di default è 20 20  
+            jButtonPlayer.setEnabled(false);                          //Lo rende non premibile, l'icona del player è un pulsante che non 
+            jButtonPlayer.setOpaque(false);
+            //Aggiungo le label e il pulsante del player nella posizione corretta, è forzato essere O(n^2)
             int i=0; int ii=0;
-            for( i=0; i<20; i++){
+            for( i=0; i<20; i++)
+            {
                 for( ii=0; ii<20; ii++)
                 {
-                    if(ii== x&& i==y){jLabelMap.add(jButtonPlayer);}
-                    else{
+                    if(ii== x&& i==y)
+                    {
+                        jLabelMap.add(jButtonPlayer);
+                    }else
+                    {
                         JLabel l=new JLabel(" ");
-                    l.setForeground(Color.RED);
-                    jLabelMap.add(l);
-                        }
+                        l.setForeground(Color.RED);
+                        jLabelMap.add(l);
+                    }
                 }
             }
-
             jInternalFrame1.add(jLabelMap);
             jInternalFrame1.setVisible(true);
-
-         
-
+            }catch(Exception e){System.err.println("Errore");}
         }
 
         /**
-         * Sets the postion of the player to the default one
+         * Sets the postion of the player to the DEFAULT one
          * @param x The position from the top left corner along the x axis
          * @param y The position from the top left corner along the y axis
          * @param PlayerImage is the path to the icon of the player 
+         * note: Questo metodo è ora oblsoleto
         */
+
         private void addPlayerPosition(int x, int y, String PlayerImage) {
 
 
@@ -211,8 +108,8 @@ public class GameFrame extends javax.swing.JFrame implements Frame {
 
            //  jButtonPlayer.setIcon(imageIcon);
              try {
-                addMapPicture(MapGraph.getPathImage(new Coordinates(x,y)));
-                move(x,y);
+              //  addMapPicture(MapGraph.getPathImage(new Coordinates(x,y)));
+                move(new Coordinates(x,y));
             } catch (RuntimeException e) {
                 e.printStackTrace();
             }
@@ -220,27 +117,17 @@ public class GameFrame extends javax.swing.JFrame implements Frame {
         }
         
         /**
-         * Adds the picture of the map to the frame
-         * @param path the path of the picture
+         * 
+         * @param c the node you want the image of 
          */
-        private void addMapPicture(String path) {
-            /*
-            jInternalFrame1.getContentPane().setLayout(new BorderLayout());
-            jInternalFrame1.setSize(500,500);
-            //jButtonPlayer.setForeground(new java.awt.Color(255, 255, 255));
-            
-            
-            jLabelMap.setSize(jInternalFrame1.getSize(getSize()));
-           
-          
-            jLabelMap.setLayout(new GridLayout(20,20));
-            jInternalFrame1.add(jLabelMap);
-            SwingUtilities.updateComponentTreeUI(jInternalFrame1);*/
-            jButtonPlayer.setOpaque(false);
-            jButtonPlayer.setVisible(true);
-            path = ParsePath.getPath(imageDirPath, path);
-            jLabelMap.setIcon(new ImageIcon(path));
+        public void updateGraphics(Coordinates c )
+        {
+            String path = MapGraph.getIconOf(c); //Aggiungi il percorso del nuovo nodo per avere l'immagine corretta 
+                   path = pathParser.getPath(imageDirPath,path );
+                   jLabelMap.setIcon(new ImageIcon(path));
         }
+
+
         /**
          * @param amount the value of the bar [0,100]
          * @throws RuntimeException thrown if amount is grater 100. If negative amount is set to 0 
@@ -434,7 +321,115 @@ public class GameFrame extends javax.swing.JFrame implements Frame {
        
        add(JButtonMusic);
 
-}
+    }
+    
+    /**
+     * Metodo che inizializza tutte le componenti del frame
+     */
+    public void initComponents() {
+        //Inizializzazione delle variabili oggetto per la grafica 
+        //setBackground(new java.awt.Color(0,0,0));
+        jToolBar1 = new javax.swing.JToolBar();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        jInternalFrame1 = new javax.swing.JInternalFrame("The cave");
+        jButtonPlayer = new javax.swing.JRadioButton();
+        jLabelMap = new javax.swing.JLabel();
+        jPanelProgressBars = new javax.swing.JPanel(); jPanelProgressBars.setBackground(backGround);
+        jBarVitaNemico = new javax.swing.JProgressBar();
+        jLabel1 = new javax.swing.JLabel();
+        jLabelVita = new javax.swing.JLabel();
+        jBarVitaPlayer = new javax.swing.JProgressBar();
+        jLabel2 = new javax.swing.JLabel();
+        jBarPeso = new javax.swing.JProgressBar();
+        jPanel2 = new javax.swing.JPanel();
+        jButtonInvio = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextStory = new javax.swing.JTextArea();
+        jTextComandi = new javax.swing.JTextField();
+
+        jToolBar1.setRollover(true);
+
+        //Implementazione dell'inventario
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(true);
+        jLabel1.setText("Nemico");
+        jLabelVita.setText("Vita");
+        jLabel2.setText("Peso");
+        jInternalFrame1.setVisible(true);
+        jButtonPlayer.setEnabled(false);    
+        jBarPeso.setBackground(lightBackground);
+        jBarVitaNemico.setBackground(lightBackground);
+        jBarVitaNemico.setForeground(new Color(255,0,0));
+        jBarVitaPlayer.setBackground(lightBackground);
+        jBarVitaNemico.setForeground(new Color(255,0,0));
+
+        jBarPeso.setForeground(new java.awt.Color(0, 255, 0));
+        jBarPeso.setValue(10);
+
+
+        AddComponents1(); //Metodo black box
+        
+        jTextComandi.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (jTextComandi.getText().equals("Enter your answer")) {
+                    jTextComandi.setText("");
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (jTextComandi.getText().isEmpty()) {
+                    jTextComandi.setText("Enter your answer");
+                }
+            }
+        });
+
+
+        jButtonInvio.setText("Submit");
+        jButtonInvio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                
+                try {
+                    if(jTextComandi.getText().contains("Open"))
+                    {
+                        String[] t=jTextComandi.getText().split(",");
+                        int x=Integer.parseInt(t[0]);
+                        int y=Integer.parseInt(t[1]);
+                        MapGraph.setBeaten(new Coordinates(x,y));
+                        key.setVisible(false);
+                        key.setText(MapGraph.keys+"");
+                        key.setVisible(true);                        
+                    }
+                    String[] t=jTextComandi.getText().split(",");
+                    int x=Integer.parseInt(t[0]);
+                    int y=Integer.parseInt(t[1]);
+                    move(new Coordinates(x,y));
+                    updateGraphics(new Coordinates(x,y));
+                }catch (Exception e ){}
+                
+                SwingUtilities.updateComponentTreeUI(jInternalFrame1);
+                
+           }
+        });
+
+        jTextStory.setBackground(new Color(0,0,0));
+        jTextStory.setForeground(new Color (255,255,255));
+        jTextStory.setFont(jTextStory.getFont().deriveFont(fontSize)); 
+        jTextStory.setColumns(20);
+        jTextStory.setRows(5);
+        jTextStory.setText(MapGraph.getAllIcons());
+        jScrollPane1.setViewportView(jTextStory);
+
+        jTextComandi.setText("Enter your answer");
+
+        addComponents2();
+        pack();
+    }// </editor-fold>
+
+
+
      // Variables declaration - do not modify                     
      private javax.swing.JProgressBar jBarPeso;
      private javax.swing.JProgressBar jBarVitaNemico;
