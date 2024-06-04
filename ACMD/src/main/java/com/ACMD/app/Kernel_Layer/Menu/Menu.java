99581a2f357d25eaad636cc4d9cb9ee476ee5351
CarloMaggi <quanitogame@gmail.com>
2024-06-04 17:12:00 +0200

    sto lavorando sui menu

--- ACMD/src/main/java/com/ACMD/app/Kernel_Layer/Menu/Menu.java
+++ ACMD/src/main/java/com/ACMD/app/Kernel_Layer/Menu/Menu.java
@@ -1,11 +1,13 @@
 package com.ACMD.app.Kernel_Layer.Menu;
 
 import java.util.Map;
-import java.util.Vector;
+import java.util.Vector;        //comodità per ottenere i dati
+
+import com.ACMD.app.Kernel_Layer.Prompt.CommandPattern.Command;
 
 public abstract class Menu 
 {
-    private Map<String, String> commandMap; //chiave: nome comando ; valore: descrizione
+    private Map<MenuVoice, Command> commandMap; //chiave: nome comando ; valore: descrizione
   
     public Vector<String> getElement()
     {
@@ -17,8 +19,17 @@ public abstract class Menu
 
         Object keys[]= (commandMap.keySet() ).toArray();
 
-        for(int i=0; i<keys.length; i++)    //stupidi noi che vogliamo usare una mappa al posto di un array con 2 stringhe
+        for(int i=0; i<keys.length; i++)    //stupidi noi che vogliamo usare una mappa al posto di un array con 2 stringhe -> e infatti cosa abbiamo finito per fare? Una mappa ancora più complessa che usa una classe a 2 entrate
          s+= (String)(keys[i])+'\n';        //? perché sta volta non rompre per il cast
         return s;
+    }
+
+    private class MenuVoice
+    {
+        String voice, description;      //dei comandi
+        public int hashCode()
+        {
+            return voice.hashCode();    //i comandi devono essere unici, quindi nessun problema di conflitti
+        }
     }
 }
--- ACMD/src/main/java/com/ACMD/app/Kernel_Layer/Prompt/CommandPattern/Command.java
+++ ACMD/src/main/java/com/ACMD/app/Kernel_Layer/Prompt/CommandPattern/Command.java
@@ -3,5 +3,5 @@ package com.ACMD.app.Kernel_Layer.Prompt.CommandPattern;
 
 public interface Command{
 
-    void execute(); //#TERMINARE : vedere il pattern command che vuole 2 metodi
+    void execute();
 }
