0afe379bdc9b51ab42cb473f1fa1c7d5445930ed
PdP03 <40737069+PdP03@users.noreply.github.com>
2024-06-06 22:41:39 +0200

    Added checksum check

--- ACMD/src/main/java/com/ACMD/app/Save_Layer/awsClient.java
+++ ACMD/src/main/java/com/ACMD/app/Save_Layer/awsClient.java
@@ -1,46 +1,55 @@
 package com.ACMD.app.Save_Layer;
 
+import java.io.IOException;
+
+
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectAttributesRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectAttributesResponse;
+import software.amazon.awssdk.services.s3.model.ObjectAttributes;
 import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.core.sync.RequestBody;
+
 
 
 import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 
 public class awsClient {
 
-    public awsClient() {}
-
-    final static Region region = Region.US_EAST_1;
-    final static String bucketName = "macdtest";
-    final static String accessKeyId = "AKIAZJ42G7ZVBUEAUGE3";
-    final static String secretAcessKey = "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";
-
-    static AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKeyId, secretAcessKey);
+    private final static Region region = Region.US_EAST_1;
+    private final static String bucketName = "macdtest";
+    private final static String accessKeyId = "AKIAZJ42G7ZVBUEAUGE3";
+    private final static String secretAcessKey = "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";
+    private final static AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKeyId, secretAcessKey);
+    private final static S3Client client = S3Client.builder().region(region)
+            .credentialsProvider(StaticCredentialsProvider.create(awsCreds)).build();
 
     public static void Upload(File file) {
 
-        
-        S3Client client = S3Client.builder()
-        .region(region)
-        .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-        .build();
-        DateTimeFormatter dt = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
+        DateTimeFormatter dt = DateTimeFormatter.ofPattern("yyyy-MM-dd:HH-mm");
 
         try {
-            PutObjectRequest request = PutObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(dt.format(LocalDateTime.now()))
+            PutObjectRequest request = PutObjectRequest.builder().bucket(bucketName).key(dt.format(LocalDateTime.now()))
                     .build();
             client.putObject(request, RequestBody.fromFile(file));
-            System.out.println("Game file Uploaded"); //Da modificare per stampare sul "terminale" di gioco secondo i metodi della GUI
             client.close();
         } catch (S3Exception e) {
             System.err.println(e.awsErrorDetails().errorMessage());
@@ -48,21 +57,43 @@ public class awsClient {
         }
     }
 
-    public void Download(String fileName){
-        S3Client client = S3Client.builder()
-        .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-        .region(region)
-        .build();
+    public void Download(int index) {
+        List<String> saveFiles = GetUploadedFiles();
         try {
-            GetObjectRequest request =  GetObjectRequest.builder().bucket(bucketName).key(fileName).build();
+            GetObjectRequest request = GetObjectRequest.builder().bucket(bucketName).key(saveFiles.get(index)).build();
             client.getObject(request);
-            
+
         } catch (S3Exception e) {
             System.err.println(e.awsErrorDetails().errorMessage());
             client.close();
         }
     }
 
- 
+    public List<String> GetUploadedFiles() {
+        ListObjectsV2Request listRequest = ListObjectsV2Request.builder().bucket(bucketName).build();
+
+        ListObjectsV2Response listResponse = client.listObjectsV2(listRequest);
+
+        return listResponse.contents().stream().map(S3Object::key).collect(Collectors.toList());
+    }
+
+    private boolean CheckSum(int index) {
+        List<String> saveFiles = GetUploadedFiles();
+
+        GetObjectAttributesRequest attrReq = GetObjectAttributesRequest.builder().bucket(bucketName).key(saveFiles.get(index))
+                .objectAttributes(ObjectAttributes.E_TAG).build();
+
+        GetObjectAttributesResponse attrRes = client.getObjectAttributes(attrReq);
+
+        String remoteMD5 = attrRes.eTag().replace("\"", "");
+        try (InputStream is = Files.newInputStream(Paths.get("Savefile.json"))) {
+            String localMD5 = DigestUtils.md5Hex(is);
+            return localMD5.equals(remoteMD5);
+        } catch(IOException e){
+            e.printStackTrace();
+            return false;
+        }
+
+    }
 
 }
