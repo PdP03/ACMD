a60c8f959394191931f0eb799b17a6ebc6eb75a6
X3laRam <169063754+X3laRam@users.noreply.github.com>
2024-06-06 19:03:41 +0200

    prova hashmap

--- ACMD/src/main/java/com/ACMD/app/Engine_Layer/GameEngine/main2.java
+++ ACMD/src/main/java/com/ACMD/app/Engine_Layer/GameEngine/main2.java
@@ -1,7 +1,10 @@
 package com.ACMD.app.Engine_Layer.GameEngine;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Vector;
+
+import javax.xml.crypto.dsig.spec.HMACParameterSpec;
 
 import com.ACMD.app.Engine_Layer.ParsePath;
 import com.ACMD.app.Engine_Layer.xmlReader;
@@ -23,8 +26,8 @@ public class main2 {
                 //System.out.println(values.size());
                 for(MenuValues menuItem: values){
 
-                    System.out.println(menuItem.cmdDescription);
-                    System.out.println(menuItem.cmdName);
+                    //System.out.println(menuItem.cmdDescription);
+                    //System.out.println(menuItem.cmdName);
                 }
             }
         }
@@ -34,10 +37,45 @@ public class main2 {
         Vector<String> files = ParsePath.getFilesNameIn(dir);
 
         for(String file: files){
-            System.out.println(file);
-            System.out.println((new File(thisDir+file)).exists());
+            //System.out.println(file);
+            //System.out.println((new File(thisDir+file)).exists());
         }
+
+        HashMap<entry, Integer> map = new HashMap<>();
+        
+        entry e = new entry("cio","cio");
+        entry e2 = new entry("ciao","ciao");
+
+        map.put(e, 1);
+        map.put(e2, 2);
+
+        System.out.println(map.containsKey(new entry("cio","cio")));
+        System.out.println(map.containsKey(new entry("ciao","cia")));
+
+        System.out.println(e.hashCode());
+        System.out.println(e2.hashCode());
 
     }
     
+}
+
+class entry{
+    public String s1;
+    public String s2;
+
+    public entry(String s1, String s2){
+        this.s1 = s1;
+        this.s2 = s2;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        entry e = (entry)o;
+        return s1 == e.s1;
+    }
+
+    @Override
+    public int hashCode(){
+        return s1.hashCode();
+    }
 }
