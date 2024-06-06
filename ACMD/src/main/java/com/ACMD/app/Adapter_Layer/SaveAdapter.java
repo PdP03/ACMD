82c1cc3f84a36c8160577866508e60c278b24f6d
PdP03 <40737069+PdP03@users.noreply.github.com>
2024-06-06 23:12:19 +0200

    Added code to the correct file

--- ACMD/src/main/java/com/ACMD/app/Adapter_Layer/SaveAdapter.java
+++ ACMD/src/main/java/com/ACMD/app/Adapter_Layer/SaveAdapter.java
@@ -1,6 +1,29 @@
 package com.ACMD.app.Adapter_Layer;
 
+import java.io.IOException;
+
+import com.ACMD.app.Engine_Layer.Entita.Player;
+import com.ACMD.app.Engine_Layer.Mappa.MapGraph;
+import com.ACMD.app.Save_Layer.JsonParser;
+import com.google.gson.JsonObject;
+
 public class SaveAdapter
 {
-    
+    public static void save(Player player){
+        JsonParser.save(player);
+    }
+
+    public static void save (MapGraph map){
+        JsonParser.save(map);
+    }
+
+    public static Player retrivePlayer(){
+        Player player = JsonParser.read(Player.class);
+        return player;
+    }
+
+    public static MapGraph retriveMap(){
+        MapGraph map = JsonParser.read(MapGraph.class);
+        return map;
+    }
 }
