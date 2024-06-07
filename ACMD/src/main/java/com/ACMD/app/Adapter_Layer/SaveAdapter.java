94b38bac4c5ae32b195e6cdb8937ad4d0d73f112
PdP03 <40737069+PdP03@users.noreply.github.com>
2024-06-07 12:36:35 +0200

    Refactor SaveAdapter class for better code organization and documentation

--- ACMD/src/main/java/com/ACMD/app/Adapter_Layer/SaveAdapter.java
+++ ACMD/src/main/java/com/ACMD/app/Adapter_Layer/SaveAdapter.java
@@ -7,21 +7,45 @@ import com.ACMD.app.Engine_Layer.Mappa.MapGraph;
 import com.ACMD.app.Save_Layer.JsonParser;
 import com.google.gson.JsonObject;
 
+/**
+ * This class is used to save and retrieve the player and the map from the Json file.
+ */
+
 public class SaveAdapter
 {
+    /**
+     * This method saves the player to the Json file.
+     * @param player The player to save.
+     */
+
     public static void save(Player player){
         JsonParser.save(player);
     }
 
+    /**
+     * This method saves the map to the Json file.
+     * @param map The map to save.
+     */
+
     public static void save (MapGraph map){
         JsonParser.save(map);
     }
+    
+    /**
+     * This method retrieves the player from the Json file.
+     * @return The player.
+     */
 
     public static Player retrivePlayer(){
         Player player = JsonParser.read(Player.class);
         return player;
     }
-
+    
+    
+    /**
+     * This method retrieves the map from the Json file.
+     * @return The map.
+     */
     public static MapGraph retriveMap(){
         MapGraph map = JsonParser.read(MapGraph.class);
         return map;
