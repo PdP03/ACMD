830f40c8280d12359681af1c9e1bca499f4ca08a
X3laRam <169063754+X3laRam@users.noreply.github.com>
2024-06-08 14:58:50 +0200

    pu

--- ACMD/src/main/java/com/ACMD/app/Engine_Layer/GameEngine/main4.java
+++ ACMD/src/main/java/com/ACMD/app/Engine_Layer/GameEngine/main4.java
@@ -5,10 +5,12 @@ import java.io.File;
 
 public class main4 {
     public static void main(String[] args) {
-        System.out.println(ParsePath.getPath("\\ACMD\\src\\main\\java\\com\\ACMD\\app\\Engine_Layer\\StorageManagement\\", "ItemStackConfig.xml"));
-        File file = new File(ParsePath.getPath("\\ACMD\\src\\main\\java\\com\\ACMD\\app\\Engine_Layer\\StorageManagement\\", "ItemStackConfig.xml"));   
+
+        String path = ParsePath.getPath("\\ACMD\\src\\main\\java\\com\\ACMD\\app\\Engine_Layer\\StorageManagement\\", "ItemStackConfig.xml");
+        System.out.println(path);
+        File file = new File(path);   
         System.out.println(file.exists());
-        xmlReader reader = new xmlReader("\\ACMD\\src\\main\\java\\com\\ACMD\\app\\Engine_Layer\\StorageManagement\\", "ItemStackConfig.xml");
+        xmlReader reader = new xmlReader("\\ACMD\\src\\main\\java\\com\\ACMD\\app\\Engine_Layer\\StorageManagement\\","ItemStackConfig.xml");
         System.out.println(reader.getAllItemStack()); 
     }
 }
