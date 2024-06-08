9ae7a8edc32075c44173714d3a7e7563c2164150
CarloMaggi <quanitogame@gmail.com>
2024-06-08 17:18:11 +0200

    cambiato output in take

--- ACMD/src/main/java/com/ACMD/app/Kernel_Layer/Prompt/command/Command_Stanze/TakeFromChest.java
+++ ACMD/src/main/java/com/ACMD/app/Kernel_Layer/Prompt/command/Command_Stanze/TakeFromChest.java
@@ -28,6 +28,7 @@ public class TakeFromChest implements Command
         if( gme.canPlayerTake(struments.elementAt(1)) )
          {
             gme.playerTake(struments.get(1));
+            gme.addBuffer("Hai inserito in inventario "+ struments.get(1));
          }
         
 
