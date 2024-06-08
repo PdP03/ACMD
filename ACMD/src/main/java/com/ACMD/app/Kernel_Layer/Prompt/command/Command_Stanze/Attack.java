5a156a45c277480e4c232d5d1ca2cd3a2433867b
CarloMaggi <quanitogame@gmail.com>
2024-06-08 14:59:02 +0200

    devo pullare_sto sistemando attack

--- ACMD/src/main/java/com/ACMD/app/Kernel_Layer/Prompt/command/Command_Stanze/Attack.java
+++ ACMD/src/main/java/com/ACMD/app/Kernel_Layer/Prompt/command/Command_Stanze/Attack.java
@@ -30,11 +30,13 @@ public class Attack implements Command
         if( nothing.size() > 1 )
         return BackStateGame_Enum.ERROR_DIGIT;     //qualcosa che non và
 
-        if( gme.isPlayerInRoom() )  //controllo non necessario, ma se per qualche motivo accade c'è un problema
+        if( ! gme.isPlayerInRoom() )  //controllo non necessario, ma se per qualche motivo accade c'è un problema
         {                           //tanto sarebbe una azione rara, quindi non importa per il controllo... se è per una cosa così critica  (altrimenti dovrei cercare di stare sempre in stati validi ed evitare if che possono cambiare)
             System.out.println("DEBUG: la voce è disponibile quando non si è in una stanza");
             System.exit(1);
         }
+
+        gme.playerCanAttack();
 
     try{
         while( true )
